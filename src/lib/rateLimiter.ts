69bc711a22599b50dbedf1fd6eea6008c8119deb
theRealadwaidh <adwaidhcke2@gmail.com>
2025-06-19 10:35:44 +0530

    removed timer

--- src/lib/rateLimiter.ts
+++
@@ -1,8 +0,0 @@
-//src/lib/rateLimiter.ts
-
-import { RateLimiterMemory } from 'rate-limiter-flexible';
-
-export const rateLimiter = new RateLimiterMemory({
-points: 5, // 5 requests
-duration: 60, // per 60 seconds per IP
-});
